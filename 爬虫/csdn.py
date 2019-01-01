a00dd1828aa83d1d2755b092af69a38d4cc46faa
cz9025 <897308902@qq.com>
2019-01-01 17:21:33 +0800

    爬虫csdn

--- 爬虫/csdn.py
+++ 爬虫/csdn.py
@@ -34,6 +34,23 @@ def get_py(res):
             all_title.append(mycsdn)
 
 
+def get_info(res):
+    res = res.result()
+    print res
+    # print res['text']
+    html = etree.HTML(res['text'])
+    # 不包含特定元素的，这里排除为隐藏的元素；div的列表
+    article = html.xpath('//*[@id="mainBox"]/main/div[2]/div[not(@style="display: none;")]/h4/a')
+    content = html.xpath('//*[@id="mainBox"]/main/div[2]/div[not(@style="display: none;")]/p/a')
+    for i in range(len(article)):
+        mycsdn = {}
+        mycsdn[res['name']] = article[i].xpath('./text()')[1].strip()
+        mycsdn['urls'] = article[i].xpath('./@href')[0]
+        mycsdn['cont'] = content[i].xpath('./text()')[0].strip()
+        all_title.append(mycsdn)
+    # print all_title
+
+
 def get_rfs(res):
     res = res.result()
     html = etree.HTML(res)
@@ -94,15 +111,15 @@ def main():
     with ThreadPoolExecutor(10) as p:
         uri = "https://blog.csdn.net/cz9025/article/category/"
         # python
-        p.submit(get_index, uri + '6810218', 'python').add_done_callback(get_py)
+        p.submit(get_index, uri + '6810218', 'python').add_done_callback(get_info)
         # RF+SE
-        p.submit(get_index, uri + '6810219', 'rfs').add_done_callback(get_py)
+        # p.submit(get_index, uri + '6810219', 'rfs').add_done_callback(get_py)
         # # JMETER
-        p.submit(get_index, uri + '6858016', 'jmeter').add_done_callback(get_py)
+        # p.submit(get_index, uri + '6858016', 'jmeter').add_done_callback(get_py)
         # # 接口测试
-        p.submit(get_index, uri + '6966010', 'interface').add_done_callback(get_py)
+        # p.submit(get_index, uri + '6966010', 'interface').add_done_callback(get_py)
         # # 功能测试
-        p.submit(get_index, uri + '6560600', 'functions').add_done_callback(get_py)
+        # p.submit(get_index, uri + '6560600', 'functions').add_done_callback(get_py)
 
 
 if __name__ == '__main__':
@@ -112,12 +129,13 @@ if __name__ == '__main__':
         for k in blog:
             # print
             if k == 'python':
-                print blog[k], blog['urls']
-            elif k == 'rfs':
-                print blog[k], blog['urls']
-            elif k == 'jmeter':
-                print blog[k], blog['urls']
-            elif k == 'interface':
-                print blog[k], blog['urls']
-            elif k == 'functions':
-                print blog[k], blog['urls']
+                pass
+                # print blog[k], blog['urls']
+            # elif k == 'rfs':
+            #     print blog[k], blog['urls']
+            # elif k == 'jmeter':
+            #     print blog[k], blog['urls']
+            # elif k == 'interface':
+            #     print blog[k], blog['urls']
+            # elif k == 'functions':
+            #     print blog[k], blog['urls']
