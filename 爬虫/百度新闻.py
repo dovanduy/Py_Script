84cc2c460bf70cbf00b95026072ccf501ca08806
cz9025 <897308902@qq.com>
2019-01-12 00:03:47 +0800

    baid

--- 爬虫/百度新闻.py
+++ 爬虫/百度新闻.py
@@ -7,24 +7,12 @@ from pyquery import PyQuery
 # https://www.thepaper.cn/channel_masonry.jsp?channelID=25950
 # https://www.thepaper.cn/load_index.jsp?&pageidx=2&lastTime=1542790579509
 # 百度新闻
-response = requests.get('https://news.baidu.com/guonei')
-# print response.text
-html = etree.HTML(response.text)
+response = requests.get('https://news.baidu.com')
 
-# 最新新闻
-result = html.xpath('//ul[contains(@class,"ulist")]//a')
-print len(result)
-for li in result:
-    print li.xpath('./text()')[0].encode('utf8'),
-    print li.xpath('./@href')
-# newurl = result[0].xpath('.//a/@href')[0]
-# print newurl
-# response = requests.get(newurl)
+pq=PyQuery(response.text)
+top= pq("#pane-news>div>ul").find("li")
+bot= pq("#pane-news>ul:first").find("li")
+strs=top+bot
+for i in strs.items():
+    print i.find("a").html(),i.find("a").attr("href")
 
-
-# 这个只适合解析html
-pq = PyQuery(url='https://news.baidu.com/guonei')
-lista = pq('.ulist a')
-print len(lista)
-for a in lista.items():
-    print a.text(), a.attr('href')
